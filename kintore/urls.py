96c618c8a3b88b5be909c4533b29dbf3240caba3
fumitoshi <fumi10_k@yahoo.co.jp>
2020-05-06 18:09:50 +0900

    a

--- kintore/urls.py
+++ kintore/urls.py
@@ -6,5 +6,5 @@ app_name='kintore'
 urlpatterns=[
     path('',views.MealListView.as_view(),name='meallist'),
     path('meal_create/',views.MealCreateView.as_view(),name='meal_create'),
-    path('meal_graph/',views.MealGraphView.as_view(),name='meal_graph'),
+    path('meal_graph/',views.view_plot,name='view_plot'),
 ]
--- kintore/views.py
+++ kintore/views.py
@@ -5,6 +5,7 @@ from .forms import MealForm
 from django.urls import reverse_lazy
 import matplotlib.pyplot as plt
 from PIL import Image
+from django_pandas.io import read_frame
 # Create your views here.
 class MealListView(ListView):
     model=Meal
@@ -18,15 +19,38 @@ class MealCreateView(CreateView):
     form_class=MealForm
     success_url=reverse_lazy('kintore:meallist')
 
+def view_plot(request):
+    meal_data=Meal.objects.all()
+    image=read_frame(meal_data,fieldnames=['date','weight','calorie_intake','protein'])
+    plt.hist(image)
+    response=HttpResponse(content_type="image/PNG")
+    image.save(response,"PNG")
+    return response
+
+
+
+
+
+
+
+'''
 class MealGraphView(TemplateView):
-    template_name="graph.html"
-    
+    template_name='kintore/graph.html'
+
+    def view_plot(request):
+        meal_data=Meal.objects.all()
+        image=read_frame(meal_data,fieldnames=['date','weight','calorie','protein'])
+        image=plt.hist(image)
+        response=HttpResponse(content_type="image/png")
+        image.save(response,"PNG")
+        return response
+'''
+'''
     def MealGraph(request):
         meal_data=Meal.objects.all()
         image=read_frame(meal_data,fieldnames=['date','weight','calorie','protein'])
         response=HttpResponse(content_type="image/png")
         image.save(response,"PNG")
         return response
-        
+'''     
 
-        
