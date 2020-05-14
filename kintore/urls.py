1ec145c0ebf727863be3d455d1291b09ba3ef53f
fumitoshi <fumi10_k@yahoo.co.jp>
2020-05-14 15:05:45 +0900

    commit

--- kintore/urls.py
+++ kintore/urls.py
@@ -4,7 +4,15 @@ from . import views
 app_name='kintore'
 
 urlpatterns=[
-    path('',views.MealListView.as_view(),name='meallist'),
-    path('meal_create/',views.MealCreateView.as_view(),name='meal_create'),
-    path('meal_graph/',views.view_plot,name='view_plot'),
+  
+    #path('meal_create/',views.MealCreateView.as_view(),name='meal_create'),
+    #path('view_plot/',views.view_plot,name='view_plot'),
+    
+    #path('create_form/',views.MealCreateView.as_view(),name='create_form'),
+    #path('detail/',views.project_detail_view,name='detail'),
+    path('',views.KintoreFormView.as_view(),name='form_list'),
+    path('update/<int:pk>/',views.MealUpdateView.as_view(),name='meal_update'),
+    path('update_done/',views.update_done,name='update_done'),
+    path('delete/<int:pk>',views.MealDeleteView.as_view(),name='meal_delete'),
+    path('delete_done/',views.delete_done,name='delete_done'),
 ]
--- kintore/views.py
+++ kintore/views.py
@@ -1,56 +1,87 @@
 from django.shortcuts import render
 from .models import Meal
-from django.views.generic import CreateView,ListView,UpdateView,DeleteView,TemplateView
+from django.views.generic import CreateView,ListView,UpdateView,DeleteView,TemplateView,FormView,View
 from .forms import MealForm
 from django.urls import reverse_lazy
 import matplotlib.pyplot as plt
 from PIL import Image
 from django_pandas.io import read_frame
+from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import io
+from io import BytesIO
+import plotly.graph_objs as go
+from plotly.offline import plot
 # Create your views here.
-class MealListView(ListView):
-    model=Meal
-    template_name='kintore/meal_list.html'
 
-    def queryset(self):
-        return Meal.objects.all()
 
-class MealCreateView(CreateView):
+class KintoreFormView(FormView):
+    template_name="kintore/meal_list.html"
+    form_class=MealForm
+
+    def form_valid(self,form):
+        form.save()
+        x=[data.date for data in meal]
+        y=[data.weight for data in meal]
+        y2=[data.calorie_intake for data in meal]
+    
+
+        trace0=go.Scatter(x=x,y=y,mode='lines')
+
+        layout=go.Layout(xaxis=dict(title='data',type='date'),
+                        yaxis=dict(title='value'))
+
+        fig=dict(data=[trace0],layout=layout)
+
+        plot_html=plot(fig,output_type='div',include_plotlyjs=False)
+        context={
+            "form":self.form_class,
+            "meal":Meal.objects.all(),
+            "plot_html":plot_html,
+        }
+
+        return render(self.request,self.template_name,context)
+    
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        meal=Meal.objects.all()
+        context["meal"]=Meal.objects.all()
+        x=[data.date for data in meal]
+        y=[data.weight for data in meal]
+        y2=[data.calorie_intake for data in meal]
+    
+
+        trace0=go.Scatter(x=x,y=y,mode='lines')
+
+        layout=go.Layout(xaxis=dict(title='data',type='date'),
+                        yaxis=dict(title='value'))
+
+        fig=dict(data=[trace0],layout=layout)
+
+        plot_html=plot(fig,output_type='div',include_plotlyjs=False)
+        context["plot_html"]=plot_html
+        context["w"]=[data.weight for data in meal][-1]
+        return context
+
+
+
+class MealUpdateView(UpdateView):
     model=Meal
     form_class=MealForm
-    success_url=reverse_lazy('kintore:meallist')
+    success_url=reverse_lazy('kintore:update_done')
 
-def view_plot(request):
-    meal_data=Meal.objects.all()
-    image=read_frame(meal_data,fieldnames=['date','weight','calorie_intake','protein'])
-    plt.hist(image)
-    response=HttpResponse(content_type="image/PNG")
-    image.save(response,"PNG")
-    return response
+def update_done(request):
+    return render(request,'kintore/update_done.html')
 
+class MealDeleteView(DeleteView):
+    model=Meal
+    success_url=reverse_lazy('kintore:delete_done')
 
-
-
+def delete_done(request):
+    return render(request,'kintore/delete_done.html')
 
 
 
-'''
-class MealGraphView(TemplateView):
-    template_name='kintore/graph.html'
 
-    def view_plot(request):
-        meal_data=Meal.objects.all()
-        image=read_frame(meal_data,fieldnames=['date','weight','calorie','protein'])
-        image=plt.hist(image)
-        response=HttpResponse(content_type="image/png")
-        image.save(response,"PNG")
-        return response
-'''
-'''
-    def MealGraph(request):
-        meal_data=Meal.objects.all()
-        image=read_frame(meal_data,fieldnames=['date','weight','calorie','protein'])
-        response=HttpResponse(content_type="image/png")
-        image.save(response,"PNG")
-        return response
-'''     
+
 
